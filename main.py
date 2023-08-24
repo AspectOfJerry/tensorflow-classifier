117dbbc0f0e28d4aa31b3da73342024037f37853
Jerry <jerryfu_mc@outlook.com>
2023-08-24 16:52:14 -0400

    working with gpu

--- main.py
+++ main.py
@@ -2,42 +2,45 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
-print(tf.__version__)
-print('1: ', tf.config.list_physical_devices('GPU'))
-print('2: ', tf.test.is_built_with_cuda)
-print('3: ', tf.test.gpu_device_name())
-print('4: ', tf.config.get_visible_devices())
+print("Tensorflow version: ", tf.__version__)
+print("Available devices ", tf.config.get_visible_devices())
 
-physical_devices = tf.config.list_physical_devices('GPU')
-if len(physical_devices) > 1:
-    tf.config.experimental.set_memory_growth(physical_devices[1], True)  # Set memory growth for the GPU
+# https://discuss.tensorflow.org/t/tensorflow-not-detecting-gpu/16295/8
+physical_devices = tf.config.list_physical_devices("GPU")
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)  # Set memory growth for GPU
 
-
-# CIFAR-10 dataset example
+# CIFAR-10 example dataset
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-# Define the model architecture
+# define model architecture
 model = keras.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Conv2D(64, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Conv2D(64, (3, 3), activation="relu"),
     layers.Flatten(),
-    layers.Dense(64, activation='relu'),
+    layers.Dense(64, activation="relu"),
     layers.Dense(10)
 ])
 
-# Compile the model
-model.compile(optimizer='adam',
+# compile model
+model.compile(optimizer="adam",
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              metrics=["accuracy"])
 
-# Train the model
-with tf.device('/GPU:1'):  # use GPU
-    model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+epochs = 2
 
-# Evaluate the model
+# train with GPU
+with tf.device("/GPU:0"):
+    model.fit(train_images, train_labels, epochs=epochs, validation_data=(test_images, test_labels))
+
+# evaluate model
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
 print("\nTest accuracy:", test_acc)
+print(model.summary())
+
+input("Press Enter to exit...")
